c042e5fc3626039b26444aa7989624b731c8fd3c
VALERIY <val.janushkewitch@yandex.ru>
2024-09-18 11:54:56 +0200

    18/09

---
+++ Переменные/task1.py
@@ -0,0 +1,5 @@
+completed_HW = 12
+hours_spent = 1.5
+course_name = 'Pythone'
+time_one_task = hours_spent / completed_HW
+print(f'Курс: {course_name}, всего задач: {completed_HW}, затрачено часов: {hours_spent}, среднее время выполнения {time_one_task} часа.')
