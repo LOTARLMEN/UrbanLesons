03ab78bc3a0e9291e1036fe80254ba824661fca4
VALERIY <val.janushkewitch@yandex.ru>
2024-09-20 12:43:52 +0200

    20/09

---
+++ Additional task/module_1_7.py
@@ -0,0 +1,13 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+sorted_students = sorted(students)
+students_grades = {}
+average_grades = []
+average_grades.append(sum(grades[0]) / len(grades[0]))
+average_grades.append(sum(grades[1]) / len(grades[1]))
+average_grades.append(sum(grades[2]) / len(grades[2]))
+average_grades.append(sum(grades[3]) / len(grades[3]))
+average_grades.append(sum(grades[4]) / len(grades[4]))
+students_grades = zip(students, average_grades)
+students_grades = list(students_grades)
+print(students_grades)
