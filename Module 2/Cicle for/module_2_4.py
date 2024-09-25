c1338f4026d94a60144762842aa4d0b20dc27086
VALERIY <val.janushkewitch@yandex.ru>
2024-09-25 13:48:23 +0200

    25/09

--- Module 2/Cicle for/module_2_4.py
+++ Module 2/Cicle for/module_2_4.py
@@ -1,15 +1,22 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []
 not_primes = []
+
 for i in numbers:
-    for j in range(2, int(i**0.5) + 1):
-        if i % j != 0:
-            primes.append(i)
+    if i < 2:
+        not_primes.append(i)  # 1 не является простым
+        continue
 
-        else:
-            not_primes.append(i)
-        break
+    is_prime = True  # Предполагаем, что число простое
+    for j in range(2, int(i ** 0.5) + 1):
+        if i % j == 0:
+            is_prime = False  # Нашли делитель - не простое число
+            break
 
+    if is_prime:
+        primes.append(i)
+    else:
+        not_primes.append(i)
 
 print(f'Простые числа: {primes}'
       f'\nНе простые числа: {not_primes}')
