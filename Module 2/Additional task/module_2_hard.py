fbdf22136b36b61dc67fd26be5abaac1ba3d9fd5
VALERIY <val.janushkewitch@yandex.ru>
2024-09-26 15:52:43 +0200

    26/09

---
+++ Module 2/Additional task/module_2_hard.py
@@ -0,0 +1,13 @@
+def cipher(first_field):
+    if first_field in range(3, 21):
+        password_ = []
+        for i in range(1, first_field):
+            for j in range(1, first_field):
+                if first_field % (i + j) == 0 and i < j:
+                    password_.append(f"{i}{j}")
+        return ''.join(password_)
+    else:
+        return 'Вы ввели не правильное число!!!'
+
+
+print(cipher(int(input('Введите число от 3 до 20: '))))
