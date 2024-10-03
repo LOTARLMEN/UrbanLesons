17a00c89192eab42bc0b8bf6109483b744e10aec
VALERIY <val.janushkewitch@yandex.ru>
2024-10-03 11:53:57 +0200

    03/10

---
+++ Module_5/Attributes_and_methods_of_an_object/module_5_1.py
@@ -0,0 +1,25 @@
+class House():
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = int(number_of_floors)
+
+    def go_to(self):
+        while True:
+            try:
+                new_floor = int(input(f'Введите номер этажа, на который вы собираетесь переехать в здании "{self.name}"'
+                                      f'с колличеством этажей {self.number_of_floors}: '))
+                break
+            except ValueError:
+                print("Как вы планируете переехать на подобный этаж? Это даже не целое число!!!")
+
+        if new_floor > self.number_of_floors or new_floor <= 0:
+            print(f'Невозможно переехать на {new_floor} этаж в этом здании!!! Его не существует!!!')
+        else:
+            for i in range(1, new_floor + 1):
+                print(i)
+
+
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
+h1.go_to()
+h2.go_to()
