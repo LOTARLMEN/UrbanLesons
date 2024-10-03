c80c0f1ac9178be1ece041b3fb06a9b2f055f748
VALERIY <val.janushkewitch@yandex.ru>
2024-10-03 14:16:59 +0200

    03/10

--- Module_5/Attributes_and_methods_of_an_object/module_5_1.py
+++ Module_5/Attributes_and_methods_of_an_object/module_5_1.py
@@ -3,6 +3,47 @@ class House():
         self.name = name
         self.number_of_floors = int(number_of_floors)
 
+    def __eq__(self, other):
+        if isinstance(other, int) and isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
+
+    def __lt__(self, other):
+        if isinstance(other, int) and isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
+
+    def __le__(self, other):
+        if isinstance(other, int) and isinstance(other, House):
+            return self.number_of_floors <= other.number_of_floors
+
+    def __gt__(self, other):
+        if isinstance(other, int) and isinstance(other, House):
+            return self.number_of_floors > other.number_of_floors
+
+    def __ge__(self, other):
+        if isinstance(other, int) and isinstance(other, House):
+            return self.number_of_floors >= other.number_of_floors
+
+    def __ne__(self, other):
+        if isinstance(other, int) and isinstance(other, House):
+            return self.number_of_floors != other.number_of_floors
+
+    def __add__(self, value):
+        if isinstance(value, int):
+            self.number_of_floors += value
+            return self.number_of_floors
+
+    def __radd__(self, value):
+        return self.__add__(value)
+
+    def __iadd__(self, value):
+        return self.__add__(value)
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
+
     def go_to(self):
         while True:
             try:
--- Module_5/Special_class_methods/module_5_2.py
+++ Module_5/Special_class_methods/module_5_2.py
@@ -1,28 +1,4 @@
-class House():
-    def __init__(self, name, number_of_floors):
-        self.name = name
-        self.number_of_floors = int(number_of_floors)
-
-    def __len__(self):
-        return self.number_of_floors
-
-    def __str__(self):
-        return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
-
-    def go_to(self):
-        while True:
-            try:
-                new_floor = int(input(f'Введите номер этажа, на который вы собираетесь переехать в здании "{self.name}"'
-                                      f'с колличеством этажей {self.number_of_floors}: '))
-                if new_floor > self.number_of_floors or new_floor <= 0:
-                    print(f'Невозможно переехать на {new_floor} этаж в этом здании!!! Его не существует!!!')
-                    continue
-                else:
-                    for i in range(1, new_floor + 1):
-                        print(i)
-                break
-            except ValueError:
-                print("Как вы планируете переехать на подобный этаж? Это даже не целое число!!!")
+from Module_5.Attributes_and_methods_of_an_object.module_5_1 import House
 
 h1 = House('ЖК Эльбрус', 10)
 h2 = House('ЖК Акация', 20)
