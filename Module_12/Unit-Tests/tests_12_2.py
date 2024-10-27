611cdc4c3ceec19e1163de303494cb67e6b0872b
VALERIY <val.janushkewitch@yandex.ru>
2024-10-27 16:08:21 +0200

    27/10

---
+++ Module_12/Unit-Tests/tests_12_2.py
@@ -0,0 +1,43 @@
+import unittest
+from runner_and_tournament import Tournament, Runner
+
+
+class TournamentTest(unittest.TestCase):
+    DISTANCE_TOURNAMENT = 90
+
+    @classmethod
+    def setUpClass(cls):
+        cls.all_results = []
+
+    def setUp(self):
+        self.first_runner = Runner('Усэйн', speed=10)
+        self.second_runner = Runner('Андрей', speed=9)
+        self.third_runner = Runner('Ник', speed=3)
+
+    @classmethod
+    def tearDownClass(cls):
+        for result in TournamentTest.all_results:
+            formatted_result = {place: runner.name for place, runner in result.items()}
+            print(formatted_result)
+
+    def test_start_tournament(self):
+        self.tournament = Tournament(self.DISTANCE_TOURNAMENT, self.first_runner, self.third_runner)
+        self.result = self.tournament.start()
+        TournamentTest.all_results.append(self.result)
+        self.assertEqual(self.result[max(self.result.keys())].name, self.third_runner.name)
+
+    def test_start_tournament_2(self):
+        self.tournament = Tournament(self.DISTANCE_TOURNAMENT, self.second_runner, self.third_runner)
+        self.result = self.tournament.start()
+        TournamentTest.all_results.append(self.result)
+        self.assertEqual(self.result[max(self.result.keys())].name, self.third_runner.name)
+
+    def test_start_tournament_3(self):
+        self.tournament = Tournament(self.DISTANCE_TOURNAMENT, self.first_runner, self.second_runner, self.third_runner)
+        self.result = self.tournament.start()
+        TournamentTest.all_results.append(self.result)
+        self.assertEqual(self.result[max(self.result.keys())].name, self.third_runner.name)
+
+
+if __name__ == '__main__':
+    unittest.main()
